1059439b72fba3d18fb146712962bd7d3874f3d5
AliAamirniazi <44363947+AliAamirniazi@users.noreply.github.com>
2022-02-07 01:27:32 +0500

    update

--- client/src/components/payingManagment/PaymentListing.tsx
+++ client/src/components/payingManagment/PaymentListing.tsx
@@ -119,7 +119,7 @@ export default function PaymentListing() {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: '35%',
+    width: 400,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
